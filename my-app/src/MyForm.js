9eb411e4a296ccc5050e69db2afe68b5f696bfd7
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-29 13:11:29 +1000

    uncontrolled components in REact

--- my-app/src/MyForm.js
+++ my-app/src/MyForm.js
@@ -1,19 +1,35 @@
 import React from 'react';
+import { useRef } from 'react';
 
 const MyForm = () => {
+  const address = useRef('');
+  //   console.log(address);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    console.log('submitted');
+    // console.log(e);
+    // console.log('submitted');
+    console.log(address.current.value);
+  };
+  const handleOnChange = (e) => {
+    // console.log(e.target.value);
   };
   return (
     <form action="" className="card m-auto mt-5" onSubmit={handleSubmit}>
       <div className="mb-3">
-        <input
+        {/* <input
           type="text"
           className="form-control"
           placeholder="name"
           required
+          onChange={handleOnChange}
+        /> */}
+        <input
+          type="text"
+          className="form-control"
+          placeholder="email"
+          required
+          ref={address}
         />
       </div>
       <button className="btn btn-secondary">Submit</button>
